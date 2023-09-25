7d1ea030e7ceb9857685b4d223a1c2cb16c00fb1
Mr-Richard-B <richbradley90@gmail.com>
2023-09-25 20:04:33 +0100

    updated color and content

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 updateBG = () => {
   // console.log(document.querySelector("body").style.backgroundColor);
   const x = document.querySelector("body").style.backgroundColor;
-  document.querySelector("body").style.backgroundColor = (x === "lightgoldenrodyellow") ? "azure" : "lightgoldenrodyellow";
+  document.querySelector("body").style.backgroundColor = (x === "tangerine") ? "azure" : "tangerine";
 }
 
 document.querySelector("#myBtn").addEventListener("click", updateBG);
